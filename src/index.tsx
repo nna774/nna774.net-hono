f3688083de2a255b902bb54b853397875227ed74
Kugayama Nana <nonamea.774@gmail.com>
2024-02-16 16:13:26 +0900

    markdown render

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { marked } from 'marked'
 import { renderer } from './renderer'
 
 const app = new Hono()
@@ -7,6 +8,10 @@ app.get('*', renderer)
 
 app.get('/', (c) => {
   return c.render(<h1>Hello!</h1>)
+})
+
+app.get('/markdown', (c) => {
+  return c.html(marked("# Hello!\n\nThis is a markdown page!\n\n- One\n- Two\n- Three"))
 })
 
 export default app
