0f519f802c21459063fa48fda5caae95a988b2d7
Kugayama Nana <nonamea.774@gmail.com>
2024-02-20 14:21:13 +0900

    add ;

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
-import { Hono } from 'hono'
-import { marked } from 'marked'
-import { promises as fs } from 'node:fs'
+import { Hono } from 'hono';
+import { marked } from 'marked';
+import { promises as fs } from 'node:fs';
 
-import { renderer } from './renderer'
+import { renderer } from './renderer';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('*', renderer)
+app.get('*', renderer);
 
 app.get('/', (c) => {
-  return c.render(<h1>Hello!</h1>)
+  return c.render(<h1>Hello!</h1>);
 })
 
-app.get('/markdown', async (c) => {
-  const md = await fs.readFile('./md/test.md', 'utf-8')
-  const html = await marked(md)
-  return c.html(html)
+app.get('/markdown.html', async (c) => {
+  const md = await fs.readFile('./md/test.md', 'utf-8');
+  const html = await marked(md);
+  return c.html(html);
 })
 
-export default app
+export default app;
