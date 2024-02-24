c8c0fa1b4c9c6ee11723907cd5c1604d3fedb411
Kugayama Nana <nonamea.774@gmail.com>
2024-02-24 19:51:43 +0900

    blog article

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { serveStatic } from '@hono/node-server/serve-static';
 import { marked } from 'marked';
 import parseMD from 'parse-md';
 import { promises as fs } from 'node:fs';
+import { glob } from 'glob';
 
 import { renderer, blogRenderer } from './renderer';
 
@@ -14,13 +15,27 @@ type Page = {
 
 type Metadata = {
   title: string;
+  date?: number;
 };
+
+type MetadataString = { [key in keyof Metadata]: string };
+
+const parse = (v: string): { metadata: Metadata, content: string } => {
+  const res = parseMD(v);
+  const metadata: MetadataString = res.metadata as MetadataString;
+  const content: string = res.content;
+  return {
+    metadata: {
+      title: metadata.title,
+      date: metadata.date ? Date.parse(metadata.date) : undefined
+    },
+    content
+  };
+}
 
 const page = async (path: string): Promise<Page> => {
   const file = await fs.readFile('./src/pages/' + path, 'utf-8');
-  const res = parseMD(file);
-  const metadata: Metadata = res.metadata as Metadata;
-  const content: string = res.content;
+  const { metadata, content } = parse(file);
   return { title: metadata.title, content };
 };
 
@@ -36,7 +51,7 @@ app.use('*', async (c, next) => {
 app.get('/css/*', serveStatic({ root: './public' }))
 
 // define html pages routes
-const pages = ['index.html'];
+const pages = ['index.html']; // TODO: grobにする。
 pages.map((path) => {
   app.get('/', async (c) => {
     const p = await page(path);
@@ -44,14 +59,30 @@ pages.map((path) => {
   });
 });
 
-// define blog routes
-app.get('/blog/*', async (c) => {
-  const md = await fs.readFile('./md/test.md', 'utf-8');
-  const res = parseMD(md);
-  const metadata: Metadata = res.metadata as Metadata;
-  const content: string = res.content;
+// define blog article routes
+const blogPaths = await glob('./src/blog/**/*.md');
+const blog = await Promise.all(blogPaths.map(async (path) => {
+  const md = await fs.readFile(path, 'utf-8');
+  const { metadata, content } = parse(md);
   const h = await marked(content);
-  return c.render(raw(h), { title: metadata.title });
+  const d = path.substring(0, 10); // 2024/01/01 like
+  console.log(d);
+  console.log(metadata.date);
+  return {
+    path: path.substring(4, path.length - 3), // remove prefix src and suffix .md
+    title: metadata.title,
+    content: h,
+    date: new Date(metadata.date || Date.parse(d))
+  }
+}));
+console.log(blog);
+blog.map((article) => {
+  app.get(article.path, (c) => {
+    return c.render(raw(article.content), { title: article.title });
+  });
 });
+
+// define blog system routes
+// TODO:
 
 export default app;
