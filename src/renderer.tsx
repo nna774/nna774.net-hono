76f4c38d384510aa068da93185eee7368ae61d32
Kugayama Nana <nonamea.774@gmail.com>
2024-02-25 05:20:42 +0900

    load widgets and add comment

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -63,9 +63,10 @@ export const blogRenderer = jsxRenderer(
           <link href='/css/blog.css' rel='stylesheet' />
           <link href='/css/bootstrap.css' rel='stylesheet' />
 
-          <script>
+          <script>{/* tweet button */}
             {html`!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.async=true;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');`}
           </script>
+          <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         </head>
 
         <body class='container-fluid'>
