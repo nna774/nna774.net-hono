5cf178aa5fef3d72c4f51373c9d991ec04cef0a3
Kugayama Nana <nonamea.774@gmail.com>
2024-02-25 05:54:58 +0900

    copy to dist blog attachments

--- build.ts
+++ build.ts
@@ -1,5 +1,17 @@
 import { toSSG } from 'hono/ssg';
 import fs from 'fs/promises';
+import { glob } from 'glob';
 import app from './src/index';
 
-toSSG(app, fs);
+const outDir = './dist';
+
+toSSG(app, fs, {dir: outDir});
+
+// blogの付属品をコピーする。
+const files = await glob('./src/blog/**/*');
+files.map(async (file) => {
+  if (!file.endsWith('.md') && (await fs.stat(file)).isFile()) {
+    fs.cp(file, file.replace('src/blog/', outDir + '/blog/'));
+    console.log(file, file.replace('src/blog/', outDir + '/blog/'));
+  }
+});
