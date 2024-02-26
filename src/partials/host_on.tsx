3bb5608f2bf0414d7053f17dd58116e20e599f7b
Kugayama Nana <nonamea.774@gmail.com>
2024-02-26 18:32:11 +0900

    impl get commit data

--- src/partials/host_on.tsx
+++ src/partials/host_on.tsx
@@ -1,3 +1,8 @@
+import { execSync } from 'child_process'
+
+const revision = execSync('git log --pretty=format:%H -n1').toString().trim();
+const commitMessage = execSync('git log --pretty=format:%s -n1').toString().trim();
+
 export const HostOn = () => (
   <>
     <p id='hostOn'>
@@ -5,8 +10,8 @@ export const HostOn = () => (
       このページはGithubの<a href='https://github.com/nna774/nna774.net'>nna774/nna774.net</a>にレポジトリがあります。
     </p>
     <p id='_commitHash'>
-      このページのビルド時のCommit Hashは<a href='https://github.com/nna774/nna774.net/tree/<%= commitHash %>'>####</a>です。<br />
-      Commit メッセージは"####"です。
+      このページのビルド時のCommit Hashは<a href={'https://github.com/nna774/nna774.net/tree/' + revision}>{revision}</a>です。<br />
+      Commit メッセージは"{commitMessage}"です。
     </p>
     <p id='_buildDate'>
       ビルド時刻は{Date()}です。
