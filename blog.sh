e56f1e1bed32a926aae4597b71f7af6fdf8aa487
Kugayama Nana <nonamea.774@gmail.com>
2024-02-27 21:34:33 +0900

    copy blog.sh

---
+++ blog.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+
+YEAR=`date +"%Y"`
+MONTH=`date +"%m"`
+DAY=`date +"%d"`
+TIME=`date +"%H:%M"`
+ZONE="JST"
+
+TITLE=$1
+if [ -z "${TITLE}" ]; then
+    echo need title
+    exit 255
+fi
+
+NAME="${TITLE}.html.md"
+DATE="${YEAR}-${MONTH}-${DAY} ${TIME} ${ZONE}"
+DIR="src/blog/${YEAR}/${MONTH}/${DAY}"
+
+mkdir -p "${DIR}"
+
+ARTICLE_PATH="${DIR}/${NAME}"
+if [ -e "${ARTICLE_PATH}" ]; then
+    echo already exist!
+    exit 255
+fi
+
+echo "---" > "${ARTICLE_PATH}"
+echo "title: ${TITLE}" >> "${ARTICLE_PATH}"
+echo "date: ${DATE}"  >> "${ARTICLE_PATH}"
+echo "tags:"  >> "${ARTICLE_PATH}"
+echo "---"  >> "${ARTICLE_PATH}"
+
+echo "${ARTICLE_PATH} created"
+
+code "${ARTICLE_PATH}"
