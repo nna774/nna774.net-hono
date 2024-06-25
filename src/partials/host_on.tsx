4af5abb281c260820414eeebf822ab3b780af9bf
Kugayama Nana <nonamea.774@gmail.com>
2024-06-25 20:48:53 +0900

    :police:

--- src/partials/host_on.tsx
+++ src/partials/host_on.tsx
@@ -7,8 +7,8 @@ export const HostOn = () => (
   <>
     <p id='hostOn'>
       <del><a href='https://www.flickr.com/photos/nna774/14434965670/'>Powered with Raspberry Pi</a></del><br />
-      <del>このページはGithubの<a href='https://github.com/nna774/nna774.net'>nna774/nna774.net</a>にレポジトリがあります。</del><br />
-      このページはGithubの<a href='https://github.com/nna774/nna774.net-hono'>nna774/nna774.net-hono</a>にレポジトリがあります。<br />
+      <del>このページはGitHubの<a href='https://github.com/nna774/nna774.net'>nna774/nna774.net</a>にレポジトリがあります。</del><br />
+      このページはGitHubの<a href='https://github.com/nna774/nna774.net-hono'>nna774/nna774.net-hono</a>にレポジトリがあります。<br />
     </p>
     <p id='_commitHash'>
       このページのビルド時のCommit Hashは<a href={'https://github.com/nna774/nna774.net-hono/tree/' + revision}>{revision}</a>です。<br />
